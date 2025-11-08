9c8bf4e2897981be1b0ab434e3ca8d5113bf719f
kavyai1 <kavya.i2403@gmail.com>
2025-11-08 10:57:08 -0700

    Added code to spin robot not yet working

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rotatingGeorge.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Tools.PID;
+import org.firstinspires.ftc.teamcode.Tools.Parameters;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+
+@Autonomous
+public class rotatingGeorge extends LinearOpMode {
+    DcMotor leftFront;
+    DcMotor rightFront;
+    DcMotor leftBack;
+    DcMotor rightBack;
+    public IMU imu;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class,"leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        PID drivePID = new PID(0.5,0,0);
+        double target= 9257;
+        drivePID.setSetPoint(target);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.RIGHT;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+
+        imu.initialize(new IMU.Parameters(orientationOnRobot));
+        imu.resetYaw();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+            double yaw = orientation.getYaw(AngleUnit.DEGREES);
+            double pitch = orientation.getPitch(AngleUnit.DEGREES);
+            double roll = orientation.getRoll(AngleUnit.DEGREES);
+
+            drivePID.updatePID(leftFront.getCurrentPosition());
+            drivePID.updatePID(rightFront.getCurrentPosition());
+            drivePID.updatePID(leftBack.getCurrentPosition());
+            drivePID.updatePID(rightBack.getCurrentPosition());
+
+            leftFront.setPower(-target);
+            rightFront.setPower(target);
+            leftBack.setPower(target);
+            rightBack.setPower(target);
+
+            telemetry.addData("Yaw", yaw);
+            telemetry.addData("Pitch", pitch);
+            telemetry.addData("Roll", roll);
+            telemetry.update();
+        }
+    }
+}
+
+
