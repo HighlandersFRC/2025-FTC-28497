a2dd4c0fba01f3ba02450f4bfb42406a58f49e39
Havish-Seri <havish.seri@gmail.com>
2026-01-16 12:09:59 -0700

    Adding old code to new branch COMP-CODE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motor2.java
@@ -5,7 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class motor2 extends LinearOpMode {
+public class
+motor2 extends LinearOpMode {
     private DcMotor motor;
 
     @Override
